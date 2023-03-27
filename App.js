785eab031c3f088e6bdc73c442375b56b210c9ec
swetha3819 <108916586+swetha3819@users.noreply.github.com>
2023-03-27 10:35:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Team from './Team';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <Team/>
+      
+      </div>
+     
+  );
+}
+
+export default App;
